620fcdfb71bcfe87ff491b64f6494e8f3721a73c
ghkim1223 <joyqplus@gmail.com>
2019-02-18 21:43:56 +0900

    chart 내일 마저 만들어야함

---
+++ src/components/partial/Chart.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Chart extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = { }
+    }
+
+    render() {
+        return (
+            <div>
+                차트들어가거임
+
+            </div>
+        );
+    }
+
+}
+
+export default Chart;
