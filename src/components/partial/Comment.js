f6150759a0dd05da980fd54cebf160ba9f783d71
sssssungs <sssssungs@gmail.com>
2019-02-26 21:04:38 +0900

    comment 별 개수선택, 내용입력 되도록

--- src/components/partial/Comment.js
+++ src/components/partial/Comment.js
@@ -7,40 +7,65 @@ import "../partial/Comment.css";
 class Comment extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      clickedStar: 0,
+      comment: ""
+    }
+
+    this.clickStar = this.clickStar.bind(this);
+    this.inputComment = this.inputComment.bind(this);
+    this.saveComment = this.saveComment.bind(this);
   }
+
+  clickStar(e){
+    this.setState({
+      clickedStar: (this.state.clickedStar==e.target.getAttribute("data-number")) ? 0 : e.target.getAttribute("data-number") //누른거 또 누를경우 0으로
+    })
+  }
+
+  inputComment(e){
+    this.setState({
+      comment: e.target.value
+    })
+  }
+
+  saveComment(e){
+    
+    alert(`별 ${this.state.clickedStar}개이고 코멘트 ${this.state.comment} 세이브한다.`)
+    //console.log(this.state)
+  }
+
+
   render() { 
     
-    let starRate = [];
-    _.times(5, () => {
-      starRate.push(<Icon name="star" />);
-    });
-
     const divStyle={
       "padding":"5px"
     };
 
     return ( 
+      
+        <div style={{"display":"flex", "flexDirection":"column"}} >
+          <div style={divStyle}>
+            <span>{ _.times(5, (idx) => { return <Icon name="star" key={idx} />; }) }</span>
+          재미있음.
+          </div>
+          <div style={divStyle}>
+            <span>{ _.times(5, (idx) => { return <Icon name="star" key={idx} />; }) }</span>
+          뭔데이거
+          </div>
+          <div style={divStyle}>
+              <Icon name={this.state.clickedStar >= 1 ? "star" : "star outline"} data-number="1" className="pointer-cursor" onClick={this.clickStar}/>
+              <Icon name={this.state.clickedStar >= 2 ? "star" : "star outline"} data-number="2" className="pointer-cursor" onClick={this.clickStar}/>
+              <Icon name={this.state.clickedStar >= 3 ? "star" : "star outline"} data-number="3" className="pointer-cursor" onClick={this.clickStar}/>
+              <Icon name={this.state.clickedStar >= 4 ? "star" : "star outline"} data-number="4" className="pointer-cursor" onClick={this.clickStar}/>
+              <Icon name={this.state.clickedStar >= 5 ? "star" : "star outline"} data-number="5" className="pointer-cursor" onClick={this.clickStar}/>
+            <div className="comment-inline">
+              <Input placeholder='한줄평을 입력하세요' onChange={this.inputComment} style={{"width":"50em"}}/>
+              <Button color="teal" onClick={this.saveComment}><Icon name="plus"></Icon>Add</Button>
+            </div>
+          </div>
 
-    <div style={{"display":"flex", "flexDirection":"column"}} >
-      <div style={divStyle}>
-        <span>{ _.times(5, (idx) => { return <Icon name="star" key={idx} />; }) }</span>
-      재미있음.
-      </div>
-      <div style={divStyle}>
-        <span>{ _.times(5, (idx) => { return <Icon name="star" key={idx} />; }) }</span>
-      뭔데이거
-      </div>
-      <div style={divStyle}>
-        <span className="comment-inline">
-        { _.times(5, (idx) => { return <Icon name="star outline" key={idx} style={{"cursor":"pointer"}}/>; }) }
-        </span>
-        <div style={{"width":"50em", "marginLeft":"10px"}} className="comment-inline">
-          <Input placeholder='입력하시오' fluid action={{"color":"teal", "icon":"plus","content":"Add"}}/>
-        </div>
-      </div>
-
-    </div> 
+        </div> 
 
 
      );
