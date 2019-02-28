ecddf4b1fe8d45bed1daf0cba8a91f58246f9bc7
ghkim1223 <joyqplus@gamil.com>
2019-02-28 10:44:12 +0900

    차트랑 메인화면 합쳤쩡

--- src/components/content/Main.js
+++ src/components/content/Main.js
@@ -12,8 +12,80 @@ import {
     Segment,
     Grid
 } from 'semantic-ui-react'
+import { Doughnut, Line } from 'react-chartjs-2';
 import ThumbnailList from '../partial/ThumbnailList'
 
+const data = {
+    labels: ['루머의 루머의 루머', '나르코스', '바이킹스', '워킹데드', '하우스 오브 카드', '왕좌의 게임', '고담'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        // lineTension: 0.1,
+        backgroundColor:  ['red', 'pink', 'orange', 'yellow', 'lightgreen', 'lightblue', 'brown', 'gray'],  // data 개수만큼 노출됨
+        // borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        // pointBorderColor: 'rgba(75,192,192,1)',
+        // pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        // pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        // pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+
+const drama = {
+    labels: ['1월', '2월', '3월', '4월'],
+    datasets: [
+        {
+            label: '나르코스',
+            fill: false,
+            data: [40, 55, 35, 20],
+            borderColor: 'lightgreen'
+        },
+        {
+            label: '루머의 루머의 루머',
+            fill: false,
+            data: [60, 40, 20, 30],
+            borderColor: 'lightblue'
+        },
+        {
+            label: '워킹데드',
+            fill: false,
+            data: [30, 42, 48, 52],
+            borderColor: 'pink'
+        }
+    ]
+};
+
+const options={
+  legend: {
+    display: true,
+    position: 'right'
+  },
+  scales: {
+    yAxes: [{
+      ticks: {
+         max: 80,
+         min: 0,
+         stepSize: 20
+       }
+     }]
+    },
+   title: {
+    display: true,
+    text: 'recent drama trend',
+    position: 'bottom'
+   }
+};
 
 class Main extends React.Component{
 
@@ -75,7 +147,76 @@ class Main extends React.Component{
                     id:'10',
                     src :'https://cdn1.l-nutra.com/wp-content/uploads/2018/07/placeholder.png'
                 },
-            ]
+            ],
+            data : {
+                labels: ['루머의 루머의 루머', '나르코스', '바이킹스', '워킹데드', '하우스 오브 카드', '왕좌의 게임', '고담'],
+                datasets: [
+                  {
+                    label: 'My First dataset',
+                    fill: false,
+                    // lineTension: 0.1,
+                    backgroundColor:  ['red', 'pink', 'orange', 'yellow', 'lightgreen', 'lightblue', 'brown', 'gray'],  // data 개수만큼 노출됨
+                    // borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    // pointBorderColor: 'rgba(75,192,192,1)',
+                    // pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    // pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    // pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                  }
+                ]
+              },
+            drama : {
+                labels: ['1월', '2월', '3월', '4월'],
+                datasets: [
+                    {
+                        label: '나르코스',
+                        fill: false,
+                        data: [40, 55, 35, 20],
+                        borderColor: 'lightgreen'
+                    },
+                    {
+                        label: '루머의 루머의 루머',
+                        fill: false,
+                        data: [60, 40, 20, 30],
+                        borderColor: 'lightblue'
+                    },
+                    {
+                        label: '워킹데드',
+                        fill: false,
+                        data: [30, 42, 48, 52],
+                        borderColor: 'pink'
+                    }
+                ]
+            },
+            options : {
+              legend: {
+                display: true,
+                position: 'right'
+              },
+              scales: {
+                yAxes: [{
+                  ticks: {
+                     max: 80,
+                     min: 0,
+                     stepSize: 20
+                   }
+                 }]
+                },
+               title: {
+                display: true,
+                text: 'recent drama trend',
+                position: 'bottom'
+               }
+            }
         }
     }
     
@@ -85,16 +226,29 @@ class Main extends React.Component{
             <div>
                 <Segment style={{ padding: '5em 2em' }} vertical>
                     <Header as='h1'>My Favorite</Header>
-                    <ThumbnailList size={'tiny'} circular={true} list={myFavorite} />
+                    <Grid celled='internally' columns='equal' stackable>
+                        <Grid.Row textAlign='center'>
+                            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                                <ThumbnailList size={'small'} circular={true} list={myFavorite} />
+                            </Grid.Column>
+                            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+                                <div style={{"width":"50%", "height":"50%"}}>
+                                    <Doughnut ref="chart" data={data}  />
+                                </div>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </Segment>
                 <Segment style={{ padding: '5em 2em' }} vertical>
                     <Header as='h1'>Top 10</Header>
                     <ThumbnailList list={top10} />
                 </Segment>
                 <Segment style={{ padding: '5em 2em' }} vertical>
-                    <Header as='h1'>Trending Now</Header>
-                    <ThumbnailList list={top10} />
-                </Segment>
+                    <Header as='h1'>Recent Trend</Header>
+                    <div style={{"width":"50%", "height":"50%"}}>
+                        <Line ref="chart" data={drama} options={options}/>
+                    </div>
+                 </Segment>
                 <Segment style={{ padding: '0em' }} vertical>
                     <Grid celled='internally' columns='equal' stackable>
                         <Grid.Row textAlign='center'>
