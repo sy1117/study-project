0c0e019604c1ef96c3316fe4801cab300c7ca3d0
김소영(So-young Kim)/솔루션개발그룹/SK <sy1117@sk.com>
2019-03-13 20:03:17 +0900

    별점 아이콘 디자인 변경

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input, Icon, Segment, Button } from 'semantic-ui-react';
+import { Input, Icon, Segment, Button, Feed, Rating } from 'semantic-ui-react';
 import _ from 'lodash';
 
 import "./Comment.css";
@@ -90,14 +90,27 @@ class Comment extends Component {
 
     const generateComment = data => {
       return data.map((obj, idx)=>{
-        return <div style={divStyle} key={obj._id} id={obj._id} onMouseEnter={this.hovered} onMouseLeave={this.leaved} >
-                <span>{ _.times(obj.star, (i) => { return <Icon name="star" key={i} />; }) }</span>
-                <span>{ _.times(5-obj.star, (i) => { return <Icon name="star outline" key={i} />; }) }</span>
-                {obj.comment}
-                { this.state.hoveredId == obj._id ? deleteMark : undefined }
-                {/* <span style={{"float":"right"}}><Icon name="delete"></Icon></span> */}
-                {/* <span style={{"fontSize":"9pt", "color":"gray", "float":"right"}}>{`  (${obj.created.substring(0,10)})`}</span> */}
-              </div>
+        return (
+          <Feed>
+            <Feed.Event>
+              <Feed.Content>
+                <Feed.Summary>
+                  {obj.comment}
+                  <Feed.Date>1 Hour Ago</Feed.Date>
+                </Feed.Summary>
+                <Feed.Extra>
+                  <Rating icon='star' defaultRating={obj.star} maxRating={5} disabled/>
+                </Feed.Extra>
+                {/* <Feed.Meta>
+                  <Feed.Like>
+                    <Icon name='like' />
+                    4 Likes
+                  </Feed.Like>
+                </Feed.Meta> */}
+              </Feed.Content>
+            </Feed.Event>
+          </Feed>
+        )
       })
     }
 
@@ -106,14 +119,12 @@ class Comment extends Component {
         <div style={{"display":"flex", "flexDirection":"column"}} >
           { generateComment(this.props.commentData) }
           <div style={divStyle}>
-              <Icon name={this.state.clickedStar >= 1 ? "star" : "star outline"} data-number="1" className="pointer-cursor" onClick={this.clickStar}/>
-              <Icon name={this.state.clickedStar >= 2 ? "star" : "star outline"} data-number="2" className="pointer-cursor" onClick={this.clickStar}/>
-              <Icon name={this.state.clickedStar >= 3 ? "star" : "star outline"} data-number="3" className="pointer-cursor" onClick={this.clickStar}/>
-              <Icon name={this.state.clickedStar >= 4 ? "star" : "star outline"} data-number="4" className="pointer-cursor" onClick={this.clickStar}/>
-              <Icon name={this.state.clickedStar >= 5 ? "star" : "star outline"} data-number="5" className="pointer-cursor" onClick={this.clickStar}/>
+            <Rating icon='star' defaultRating={0} maxRating={5}/>
             <div className="comment-inline">
-              <Input placeholder='한줄평을 입력하세요' id="commentInput" onChange={this.inputComment} style={{"width":"50em"}}/>
-              <Button color="teal" onClick={this.saveComment}><Icon name="plus"></Icon>Add</Button>
+              <Input type="text" placeholder='한줄평을 입력하세요' id="commentInput" onChange={this.inputComment} action>
+                <input style={{width:'30em'}}/>
+                <Button color="teal" onClick={this.saveComment}><Icon name="plus"></Icon>Register</Button>
+              </Input>
             </div>
           </div>
         </div> 
